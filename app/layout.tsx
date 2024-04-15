8235e0ad51cfd5de7749d46a8cb0cfc4937609e2
Sahil Kumar <sahilkr_96@outlook.com>
2024-04-15 15:45:09 +1000

    add logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,9 +32,16 @@ export default function RootLayout({
           <ThemeProvider>
             <div className="flex h-screen flex-col md:flex-row">
               <div className="w-full flex-none px-4 md:pr-0 md:w-72">
-                <h1 className="rounded-lg text-inter font-bold text-3xl mt-6 mb-4 text-tremor-content-strong dark:text-dark-tremor-content-strong">
-                  Ausmetrics
-                </h1>
+                <div className="logo-container mt-6 mb-4 flex align-items gap-2">
+                  <img
+                    src="ausmetrics-logo.svg"
+                    alt="Ausmetrics Logo"
+                    className="w-8 h-8"
+                  />
+                  <span className="rounded-lg text-inter font-bold text-3xl  text-tremor-content-strong dark:text-dark-tremor-content-strong">
+                    Ausmetrics
+                  </span>
+                </div>
                 <Card className="p-3 mb-4 text-tremor-default text-tremor-content dark:text-dark-tremor-content overflow-x-scroll">
                   <p>
                     Explore the most important and up to date ABS statistics on
